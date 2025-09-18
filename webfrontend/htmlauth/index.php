25d3e77dfed1b625ca147787d75857262d3d4665
Electrica3025 <73669354+Electrica3025@users.noreply.github.com>
2025-09-18 11:38:23 +0200

    Update index.php
    Issue fix hol hamoed

--- webfrontend/htmlauth/index.php
+++ webfrontend/htmlauth/index.php
@@ -132,7 +132,7 @@ function ListhandlerLOX($jewishYear, $jewishMonth,$jewishDay, $daysinkislev){
 		$eventEN = "erev Shabbes Chol Hamoed Sukkot";
 		$eventHE = "ערב שבת חול המועד סוכות";
 		$event = 5; $holidayThirdList = 7;}‏
-  if ($holidaySecondList = 10 & $DayOfTheWeek == 6){
+  if ($holidaySecondList == 10 & $DayOfTheWeek == 6){
    		$eventEN = "Sabbes Chol Hamoed Sukkot";
 		$eventHE = "שבת חול המועד סוכות";
 		$event = 5; $holidayThirdList = 8;}	
@@ -325,15 +325,15 @@ function ListhandlerLOX($jewishYear, $jewishMonth,$jewishDay, $daysinkislev){
 			$eventHE = "אחרון של פסח - יזכור";
 			$event = 3;$holidayFirstList =7;}
 	}
-	if ($holidayFirstList =4 & $DayOfTheWeek == 5){
+	if ($holidayFirstList == 4 & $DayOfTheWeek == 5){
 		$eventEN = "erev Shabbes Chol Hamoed Pesach";
 		$eventHE = "ערב שבת חול המועד פסח";
 		$event = 5; $holidayThirdList = 10;}	
-	if ($holidayFirstList =4 & $DayOfTheWeek == 6){
+	if ($holidayFirstList == 4 & $DayOfTheWeek == 6){
 		$eventEN = "Sabbes Chol Hamoed Pesach";
 		$eventHE = "שבת חול המועד פסח";
 		$event = 5; $holidayThirdList = 10;}
-	if ($holidayFirstList =5 & $DayOfTheWeek == 6){
+	if ($holidayFirstList == 5 & $DayOfTheWeek == 6){
 		$eventEN = "Shabbes Erev Shvihy shel Pesach";
 		$eventHE = "שבת ערב שביעי של פסח";
 		$event = 5; $holidayThirdList = 11;}	
